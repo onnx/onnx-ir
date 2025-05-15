f9e605dd92ad3f71042b32e3387b9a5fc75bc911
Justin Chu <justinchuby@users.noreply.github.com>
2025-05-15 15:52:19 -0700

    Create CI pipelines
    Signed-off-by: Justin Chu <justinchuby@users.noreply.github.com>

---
+++ noxfile.py
@@ -0,0 +1,69 @@
+# Copyright (c) ONNX Project Contributors
+# SPDX-License-Identifier: Apache-2.0
+"""Test with different environment configuration with nox.
+
+Documentation:
+    https://nox.thea.codes/
+"""
+
+import nox
+
+nox.options.error_on_missing_interpreters = False
+
+
+COMMON_TEST_DEPENDENCIES = (
+    "expecttest>0.1.6",
+    "hypothesis",
+    "numpy",
+    "packaging",
+    "parameterized",
+    'psutil; sys_platform != "win32"',
+    "pytest-cov",
+    "pytest-randomly",
+    "pytest-subtests",
+    "pytest-xdist",
+    "pytest>7.1.0",
+    "pyyaml",
+    "types-PyYAML",
+    "typing_extensions>=4.10",
+    "ml-dtypes",
+)
+ONNX = "onnx==1.18"
+ONNX_RUNTIME = "onnxruntime==1.20.1"
+PYTORCH = "torch==2.7.0"
+TORCHVISON = "torchvision==0.22.0"
+TRANSFORMERS = "transformers==4.37.2"
+
+
+@nox.session(tags=["build"])
+def build(session):
+    """Build package."""
+    session.install("build", "wheel")
+    session.run("python", "-m", "build")
+
+
+@nox.session(tags=["test"])
+def test(session):
+    """Test onnx_ir and documentation."""
+    session.install(
+        *COMMON_TEST_DEPENDENCIES,
+        ONNX,
+    )
+    session.install(".", "--no-deps")
+    session.run("pip", "list")
+    session.run("pytest", "src", "--doctest-modules", *session.posargs)
+    session.run("pytest", "tests", *session.posargs)
+
+
+@nox.session(tags=["test-onnx-weekly"])
+def test_onnx_weekly(session):
+    """Test with ONNX weekly (preview) build."""
+    session.install(*COMMON_TEST_DEPENDENCIES)
+    session.install("-r", "requirements/ci/requirements-onnx-weekly.txt")
+    session.install(".", "--no-deps")
+    session.run("pip", "list")
+    session.run("pytest", "src", "--doctest-modules", *session.posargs)
+    session.run("pytest", "tests", *session.posargs)
+
+
+# TODO(justinchuby): Integration test with ONNX Script
