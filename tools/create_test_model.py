93b6d998853bc7d447a4a283119ffd34e76825e6
Justin Chu <justinchuby@users.noreply.github.com>
2025-05-16 19:19:14 -0700

    Create test data for round tripping tests (#18)
    Since the repo does not have enough git lfs space to store binary files,
    I removed all initializers and use textproto to create the round
    tripping tests.
    
    ---------
    
    Signed-off-by: Justin Chu <justinchuby@users.noreply.github.com>

---
+++ tools/create_test_model.py
@@ -0,0 +1,45 @@
+# Copyright (c) ONNX Project Contributors
+# SPDX-License-Identifier: Apache-2.0
+"""Turn an ONNX model into a textproto and strip all tensors.
+
+Usage:
+    python create_test_model.py <onnx_model_path>
+"""
+
+import argparse
+
+import onnx
+
+
+def strip_tensor_data(tensor: onnx.TensorProto) -> None:
+    """Strip data from the tensor proto."""
+    tensor.raw_data = b""
+    del tensor.float_data[:]
+    del tensor.int32_data[:]
+    del tensor.int64_data[:]
+    del tensor.string_data[:]
+    del tensor.uint64_data[:]
+
+
+def main():
+    parser = argparse.ArgumentParser(
+        description="Convert ONNX model to textproto and strip tensor data."
+    )
+    parser.add_argument("onnx_model", type=str, help="Path to the ONNX model file.")
+    args = parser.parse_args()
+
+    output_path = args.onnx_model.replace(".onnx", ".textproto")
+
+    # Load the ONNX model
+    model = onnx.load(args.onnx_model, load_external_data=False)
+
+    for tensor in model.graph.initializer:
+        strip_tensor_data(tensor)
+
+    # Save the model as a textproto
+    onnx.save(model, output_path)
+    print(f"Model saved as {output_path}")
+
+
+if __name__ == "__main__":
+    main()
