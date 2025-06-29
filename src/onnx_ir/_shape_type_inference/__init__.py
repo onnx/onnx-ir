1afe64a5e7d70a8ce53926252bde30605837c557
Justin Chu <justinchuby@users.noreply.github.com>
2025-06-29 16:20:47 -0700

    Create NodeInferencer
    Signed-off-by: Justin Chu <justinchuby@users.noreply.github.com>

--- src/onnx_ir/_shape_type_inference/__init__.py
+++ src/onnx_ir/_shape_type_inference/__init__.py
@@ -1,5 +1,7 @@
 """Symbolic shape inference for ONNX IR."""
 
+import abc
+from collections.abc import Collection, Sequence
 from typing import TYPE_CHECKING
 
 import numpy as np
@@ -50,4 +52,37 @@ def set_expr(shape: ir.Shape, index: int, expr: sympy.Expr | int) -> None:
     shape[index] = ir.SymbolicDim(str(expr), expr=expr)
 
 
-class NodeInferencer:
+class NodeInferencer(abc.ABC):
+    """Base class for node inferencers.
+
+    This class provides a common interface for all node inferencers.
+    """
+
+    def __init__(self, op_type: str, opsets: Collection[int], domain: str = "") -> None:
+        """Initialize the node inferencer.
+
+        Args:
+            op_type: The type of the operation.
+            opsets: A collection of ONNX opset versions supported by this inferencer.
+            domain: The domain of the operation, default is an empty string.
+        """
+        self.op_type = op_type
+        self.opsets = opsets
+        self.domain = domain
+
+    @abc.abstractmethod
+    def check(self, node: ir.Node) -> None:
+        """Check if the node is valid for this inferencer."""
+        raise NotImplementedError
+
+    @abc.abstractmethod
+    def infer(self, node: ir.Node) -> Sequence[ir.Value]:
+        """Infer the shape for the node.
+
+        Args:
+            node: The ONNX node to infer the type and shape for.
+
+        Returns:
+            A sequence of ONNX values containing the inferred shapes.
+        """
+        raise NotImplementedError
